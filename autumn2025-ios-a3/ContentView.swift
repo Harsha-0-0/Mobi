85ad84353319517aa1847a8b928be1a96b29e4a3
saab <yodchai.kovavinthaweewat@student.uts.edu.au>
2025-04-30 21:25:24 +1000

    initial Xcode project.

---
+++ autumn2025-ios-a3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  autumn2025-ios-a3
+//
+//  Created by Saab Kovavinthaweewat on 30/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ autumn2025-ios-a3/autumn2025_ios_a3App.swift
@@ -0,0 +1,17 @@
+//
+//  autumn2025_ios_a3App.swift
+//  autumn2025-ios-a3
+//
+//  Created by Saab Kovavinthaweewat on 30/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct autumn2025_ios_a3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
