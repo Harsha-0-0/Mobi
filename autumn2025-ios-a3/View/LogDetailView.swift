ab955c55bc9788687abc15db2ab4bc890c44e4be
Harsha <harshiismyname@gmail.com>
2025-05-05 22:45:50 +1000

    made initial view for log page and created log detail view

---
+++ autumn2025-ios-a3/View/LogDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  LogDetailView.swift
+//  autumn2025-ios-a3
+//
+//  Created by Harshi on 05/05/25.
+//
+
+import SwiftUI
+
+struct LogDetailView: View {
+    let date: Date
+
+        var body: some View {
+            Text("\(date)")
+        }
+}
+
+//#Preview {
+//    LogDetailView()
+//}
--- autumn2025-ios-a3/View/LogView.swift
+++ autumn2025-ios-a3/View/LogView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct LogView: View {
+    @State private var date = Date()
+    @State private var navigate = false
     var body: some View {
-        VStack {
-            Text("This is Log view.")
+        NavigationStack {
+            VStack(spacing: 24) {
+                
+                DatePicker(
+                    "Pick a date",
+                    selection: $date,
+                    displayedComponents: .date)
+                
+                .datePickerStyle(.graphical)
+                .scaleEffect(1.1)
+                .frame(maxWidth: .infinity, maxHeight: 450)
+                .padding()
+                .background(Color(.systemBackground))
+                .cornerRadius(12)
+                
+                
+                NavigationLink(destination: LogDetailView(date: date)) {
+                    Text("View Log Details")
+                        .font(.subheadline)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 4)
+                        .background(Capsule().fill(Color.blueButton))
+                    
+                }
+                Spacer()
+                    .navigationTitle("Log")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarBackButtonHidden(true)
+            }
+            .padding()
         }
     }
 }
+
 
 #Preview {
     ContentView(selection: .log)
